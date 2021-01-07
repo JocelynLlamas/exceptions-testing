3cb3f6dc9f701d48c92e88b25d8168f27e7ef7cf
JocelynLlamas <mjlldlt@hotmail.com>
2021-01-07 10:27:00 -0600

    first commit

---
+++ src/main/java/learning/oop/App.java
@@ -0,0 +1,13 @@
+package learning.oop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
